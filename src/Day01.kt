0dfaed0cd43bfae4432e21cce9ff375fec4ae95e
Rick Mann <3583774+Riari@users.noreply.github.com>
2022-12-07 22:46:48 +0000

    Update Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,6 @@
 fun main() {
     // Processes input into a list of total calories per elf in descending order
-    fun inputToElfList(input: List<String>): List<Int> {
+    fun processInput(input: List<String>): List<Int> {
         val elves = mutableListOf<Int>()
         var total = 0
         input.forEach {
@@ -19,19 +19,19 @@ fun main() {
         return elves
     }
 
-    fun part1(input: List<String>): Int {
-        return inputToElfList(input)[0]
+    fun part1(input: List<Int>): Int {
+        return input[0]
     }
 
-    fun part2(input: List<String>): Int {
-        return inputToElfList(input).slice(0..2).sum()
+    fun part2(input: List<Int>): Int {
+        return input.slice(0..2).sum()
     }
 
-    val testInput = readInput("Day01_test")
+    val testInput = processInput(readInput("Day01_test"))
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = processInput(readInput("Day01"))
     println(part1(input))
     println(part2(input))
 }
