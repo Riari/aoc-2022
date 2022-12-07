99bed4a6722b1d1d3816beed504df2c3ed395532
Rick Mann <3583774+Riari@users.noreply.github.com>
2022-12-07 21:46:34 +0000

    Complete Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
+    // Processes input into a list of total calories per elf in descending order
+    fun inputToElfList(input: List<String>): List<Int> {
+        val elves = mutableListOf<Int>()
+        var total = 0
+        input.forEach {
+            val calories = it.toIntOrNull()
+            if (calories == null) {
+                elves.add(total)
+                total = 0
+            } else {
+                total += calories.toInt()
+            }
+        }
+
+        elves.add(total)
+        elves.sortDescending()
+
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return inputToElfList(input)[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return inputToElfList(input).slice(0..2).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
