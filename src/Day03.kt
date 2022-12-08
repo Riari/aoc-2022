f0b27bf69636c0ea85844a0956c6379ae7944e29
Rick Mann <3583774+Riari@users.noreply.github.com>
2022-12-08 20:58:24 +0000

    Complete Day03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,40 +1,58 @@
+import javax.management.Query.match
+
 fun main() {
-   fun part1(input: List<String>): Int {
-      var sum = 0
+    fun part1(input: List<String>): Int {
+        var sum = 0
 
-      for (rucksack in input) {
-         val firstCompartment = mutableSetOf<Char>()
-         val secondCompartment = mutableSetOf<Char>()
+        for (rucksack in input) {
+            val firstCompartment = mutableSetOf<Char>()
+            val secondCompartment = mutableSetOf<Char>()
 
-         for ((index, item) in rucksack.withIndex()) {
-            if (index < rucksack.length / 2) {
-               firstCompartment.add(item)
-            } else {
-               secondCompartment.add(item)
+            for ((index, item) in rucksack.withIndex()) {
+                if (index < rucksack.length / 2) {
+                    firstCompartment.add(item)
+                } else {
+                    secondCompartment.add(item)
+                }
             }
-         }
 
-         // Find the intersection of the two compartment sets
-         val intersection = firstCompartment.intersect(secondCompartment)
+            // Find the intersection of the two compartment sets
+            val intersection = firstCompartment.intersect(secondCompartment)
 
-         // Iterate over each item in the intersection and add its priority to the sum
-         for (item in intersection) {
-            sum += if (item.isLowerCase()) item - 'a' + 1 else item - 'A' + 27
-         }
-      }
+            // Iterate over each item in the intersection and add its priority to the sum
+            for (item in intersection) {
+                sum += if (item.isLowerCase()) item - 'a' + 1 else item - 'A' + 27
+            }
+        }
 
-      return sum
-   }
+        return sum
+    }
 
-   fun part2(rucksacks: List<String>): Int {
-      return 0
-   }
+    fun part2(input: List<String>): Int {
+        var sum = 0
 
-   val testInput = readInput("Day03_test")
-   check(part1(testInput) == 157)
-   check(part2(testInput) == 0)
+        for (rucksack in input.chunked(3)) {
+            var firstRucksack = rucksack[0].toSet()
+            var secondRucksack = rucksack[1].toSet()
+            var thirdRucksack = rucksack[2].toSet()
 
-   val input = readInput("Day03")
-   println(part1(input))
-   println(part2(input))
+            // Find the intersection of the three rucksacks
+            val intersection = firstRucksack.intersect(secondRucksack.intersect(thirdRucksack))
+
+            // Iterate over each item in the intersection and add its priority to the sum
+            for (item in intersection) {
+                sum += if (item.isLowerCase()) item - 'a' + 1 else item - 'A' + 27
+            }
+        }
+
+        return sum
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
 }
