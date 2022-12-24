24658f500a5ede8e624a9a6be1fc5dd6850beaeb
Rick Mann <3583774+Riari@users.noreply.github.com>
2022-12-24 18:25:31 +0000

    Work on Day22 part 2

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,14 +1,19 @@
+import java.util.*
 import kotlin.math.sqrt
 
 fun main() {
     data class Vector2(var x: Int, var y: Int) {
-        operator fun plus(other: Vector2): Vector2 {
-            return Vector2(x + other.x, y + other.y)
+        operator fun plus(other: Vector2): Vector2 = Vector2(x + other.x, y + other.y)
+        operator fun minus(other: Vector2): Vector2 = Vector2(x - other.x, y - other.y)
+        operator fun times(other: Int): Vector2 = Vector2(x * other, y * other)
+
+        override fun equals(other: Any?): Boolean {
+            return other is Vector2
+                    && x == other.x
+                    && y == other.y
         }
 
-        operator fun minus(other: Vector2): Vector2 {
-            return Vector2(x - other.x, y - other.y)
-        }
+        override fun hashCode(): Int = Objects.hash(x, y)
     }
 
     // Action can be a movement or a rotation. For movements, the value is the number of spaces to move. For rotations, the value is -1 (left) or 1 (right).
@@ -16,25 +21,30 @@ fun main() {
 
     // Map is a container for the grid and actions. It also keeps track of simulation state.
     class Map(val grid: List<CharArray>, val actions: List<Action>) {
+        var width = 0; var height = 0
         var position = Vector2(0, 0)
         var facing = 0
-        val facings = listOf('e', 's', 'w', 'n')
-        val directions = mapOf(
+        val facings = mapOf(
             'e' to Vector2(1, 0),
             's' to Vector2(0, 1),
             'w' to Vector2(-1, 0),
             'n' to Vector2(0, -1),
         )
 
+        val path = mutableMapOf<Vector2, Char>()
+        val facingSymbols = listOf('>', 'v', '<', '^')
+
         // Cube mapping state
         var asCube = false
         var area = 0
         var sideLength = 0
-        var width = 0; var height = 0
         var internalCorners = mutableListOf<Vector2>()
+        val edgeMap = mutableMapOf<Pair<Vector2, Int>, Pair<Vector2, Int>>()
 
         init {
             reset()
+            width = grid.maxOf { it.size }
+            height = grid.size
         }
 
         fun reset() {
@@ -42,21 +52,104 @@ fun main() {
             facing = 0
         }
 
+        // Enables cube traversal and maps cube edge coordinates.
+        // I borrowed the algorithm from I borrowed the algorithm for this from https://gist.github.com/juj/1f09f475e01949233a2f206a0552425c.
+        // Thank you, juj!
         fun enableCube() {
             asCube = true
-            area = grid.sumOf { it.toString().trim().length }
+            area = grid.sumOf { it.filter { c -> c != ' ' }.size }
             sideLength = sqrt((area / 6).toDouble()).toInt()
-            width = grid.maxOf { it.size }
-            height = grid.size
+
+            for (y in 0 until height) {
+                for (x in 0 until width) {
+                    if ((isCellVoid(x, y) + isCellVoid(x + 1, y) + isCellVoid(x, y + 1) + isCellVoid(x + 1, y + 1)) == 1) {
+                        internalCorners.add(Vector2(x, y))
+                    }
+                }
+            }
+
+            mapCubeEdges()
+            check(true)
         }
 
-        fun print() {
-            for (line in grid) {
-                println(line)
+        private fun mapCubeEdges() {
+            for (corner in internalCorners) {
+                var clockwise = corner
+                var clockwiseHeading = getClockwiseHeading(clockwise)
+                var clockwiseFacing = getFacingFromIndex(clockwiseHeading)
+                var counterClockwise = corner
+                var counterClockwiseHeading = getCounterClockwiseHeading(counterClockwise)
+                var counterClockwiseFacing = getFacingFromIndex(counterClockwiseHeading)
+
+                while (true) {
+                    val clockwiseEnd = clockwise + clockwiseFacing * sideLength
+                    val counterClockwiseEnd = counterClockwise + counterClockwiseFacing * sideLength
+
+                    val newClockwiseHeading = getClockwiseHeading(clockwiseEnd)
+                    val newClockwiseFacing = getFacingFromIndex(newClockwiseHeading)
+                    val newCounterClockwiseHeading = getCounterClockwiseHeading(counterClockwiseEnd)
+                    val newCounterClockwiseFacing = getFacingFromIndex(newCounterClockwiseHeading)
+
+                    for (n in 1 .. sideLength) {
+                        val a = clockwise + clockwiseFacing * n
+                        val b = counterClockwise + counterClockwiseFacing * n
+                        val fromA = Pair(a, (clockwiseHeading - 1).mod(4))
+                        val toB = Pair(b, (counterClockwiseHeading - 1).mod(4))
+                        val fromB = Pair(b, (counterClockwiseHeading + 1).mod(4))
+                        val toA = Pair(a, (clockwiseHeading + 1).mod(4))
+                        edgeMap[fromA] = toB
+                        edgeMap[fromB] = toA
+                    }
+
+                    if (newClockwiseFacing != clockwiseFacing && newCounterClockwiseFacing != counterClockwiseFacing) {
+                        break
+                    }
+
+                    clockwise = clockwiseEnd
+                    clockwiseHeading = newClockwiseHeading
+                    clockwiseFacing = newClockwiseFacing
+                    counterClockwise = counterClockwiseEnd
+                    counterClockwiseHeading = newCounterClockwiseHeading
+                    counterClockwiseFacing = newCounterClockwiseFacing
+                }
             }
         }
 
-        // Steps through the list of actions and executes them.
+        private fun getClockwiseHeading(pos: Vector2): Int {
+            return getClockwiseHeading(pos.x, pos.y)
+        }
+
+        private fun getClockwiseHeading(x: Int, y: Int): Int {
+            val kind = isCellVoid(x, y) or (isCellVoid(x + 1, y) shl 1) or (isCellVoid(x, y + 1) shl 2) or (isCellVoid(x + 1, y + 1) shl 3)
+            val heading = listOf(-1, 3, 0, 0, 2, 3, -1, 0, 1, -1, 1, 1, 2, 3, 2, -1)[kind]
+            check(heading >= 0)
+            return heading
+        }
+
+        private fun getCounterClockwiseHeading(pos: Vector2): Int {
+            return getCounterClockwiseHeading(pos.x, pos.y)
+        }
+
+        private fun getCounterClockwiseHeading(x: Int, y: Int): Int {
+            val kind = isCellVoid(x, y) or (isCellVoid(x + 1, y) shl 1) or (isCellVoid(x, y + 1) shl 2) or (isCellVoid(x + 1, y + 1) shl 3)
+            val heading = listOf(-1, 2, 3, 2, 1, 1, -1, 1, 0, -1, 3, 2, 0, 0, 3, -1)[kind]
+            check(heading >= 0)
+            return heading
+        }
+
+        private fun getFacingFromIndex(index: Int): Vector2 = facings.values.toList()[index.mod(4)]
+
+        fun print() {
+            for (y in 0 until height) {
+                for (x in 0 until width) {
+                    print(path.getOrDefault(Vector2(x, y), grid[y].getOrElse(x) { ' ' }))
+                }
+                print("\n")
+            }
+
+            print("\n\n")
+        }
+
         fun follow() {
             for (action in actions) {
                 if (action.isRotation) {
@@ -64,17 +157,21 @@ fun main() {
                     continue
                 }
 
-                move(directions[facings[facing]]!!, action.value)
+                move(action.value)
+
+                this.print()
             }
         }
 
-        private fun move(direction: Vector2, distance: Int)
+        private fun move(distance: Int)
         {
             for (i in 0 until distance) {
+                path[position] = facingSymbols[facing]
+                val direction = getFacingFromIndex(facing)
                 when (cell(position + direction)) {
                     '.' -> position += direction
                     '#' -> break
-                    ' ', null -> {
+                    ' ' -> {
                         // Out of bounds.
                         if (!asCube && !tryWrap(direction)) break
                         if (asCube && !tryWrapAsCube(direction)) break
@@ -101,25 +198,21 @@ fun main() {
         }
 
         private fun tryWrapAsCube(direction: Vector2): Boolean {
-            return false
+            val destination = edgeMap.getOrDefault(Pair(position, facing), null)
+            check(destination != null)
+
+            if (cell(destination.first) == '#') return false
+
+            position = destination.first + direction
+            facing = destination.second
+
+            return true
         }
 
-        private fun row(row: Int = position.y): CharArray? {
-            return grid.getOrNull(row)
-        }
-
-        private fun cell(position: Vector2): Char? {
-            return cell(position.x, position.y)
-        }
-
-        private fun cell(x: Int = position.x, y: Int = position.y): Char? {
-            return row(y)?.getOrNull(x)
-        }
-
-        private fun isCellVoid(x: Int, y: Int): Int {
-            if (cell(x, y) == ' ') return 1
-            return 0
-        }
+        private fun row(row: Int = position.y): CharArray? = grid.getOrNull(row)
+        private fun cell(position: Vector2): Char = cell(position.x, position.y)
+        private fun cell(x: Int = position.x, y: Int = position.y): Char = row(y)?.getOrNull(x) ?: ' '
+        private fun isCellVoid(x: Int, y: Int): Int = if (cell(x, y) == ' ') 1 else 0
     }
 
     fun processInput(input: List<String>): Map {
@@ -159,10 +252,10 @@ fun main() {
     }
 
     val testInput = processInput(readInput("Day22_test"))
-    check(part1(testInput) == 6032)
-//    check(part2(testInput) == 5031)
+//    check(part1(testInput) == 6032)
+    check(part2(testInput) == 5031)
 
     val input = processInput(readInput("Day22"))
-    println(part1(input))
+//    println(part1(input))
 //    println(part2(input))
 }
