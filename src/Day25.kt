f853e67750fb43d2c2b862922ca8756ae7f51ad5
Rick Mann <3583774+Riari@users.noreply.github.com>
2022-12-28 00:28:12 +0000

    Complete Day25

---
+++ src/Day25.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val base = 5
+    val digits = "012=-"
+
+    fun snafuToDec(snafu: String): Long {
+        return snafu.fold(0) { carry, digit ->
+            (carry * base) + when(digit) {
+                '=' -> -2
+                '-' -> -1
+                else -> digit.digitToInt()
+            }
+        }
+    }
+
+    fun decToSnafu(dec: Long): String {
+        var snafu = ""
+        var value = dec
+        while (value != 0L) {
+            snafu = digits[value.mod(base)] + snafu
+            value = (value + 2).floorDiv(base)
+        }
+
+        return snafu
+    }
+
+    fun part1(input: List<String>): String {
+        var decimal = 0L
+        for (line in input) {
+            decimal += snafuToDec(line)
+        }
+
+        return decToSnafu(decimal)
+    }
+
+    val testInput = readInput("Day25_test")
+    check(part1(testInput) == "2=-1=0")
+
+    val input = readInput("Day25")
+    println(part1(input))
+}
